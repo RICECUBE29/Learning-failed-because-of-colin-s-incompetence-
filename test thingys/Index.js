c2c31e58da972bb75a4f4763eb72e37d2e0962a4
RICECUBE29 <77799324+RICECUBE29@users.noreply.github.com>
2021-01-31 15:36:08 -0500

    Add files via upload

---
+++ test thingys/Index.js
@@ -0,0 +1,11 @@
+
+// this code launches a nuke against iran
+/**
+ * 
+ * @param {String} IdkcolintoldmeIcouldnamethisanything 
+ */
+function giveInfo(IdkcolintoldmeIcouldnamethisanything) {
+    const link = `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/${IdkcolintoldmeIcouldnamethisanything}`
+    console.log(link)
+    window.alert(link)
+}
